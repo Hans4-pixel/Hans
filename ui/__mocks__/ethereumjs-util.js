930540f1782f92382b5e0a7cef3e4847dcb920bb
legobt <6wbvkn0j@anonaddy.me>
2024-10-29 12:48:04 +0000

    deps: wip: upgrade from ethereumjs-util to @ethereumjs/util

--- ui/__mocks__/ethereumjs-util.js
+++ ui/__mocks__/ethereumjs-util.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, node/no-extraneous-require
-const util = require('ethereumjs-util');
+const util = require('@ethereumjs/util');
 
 module.exports = {
   ...util,
