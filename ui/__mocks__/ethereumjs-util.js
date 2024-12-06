771ee548389eb2630a172826897ceb6c4624b11a
legobt <6wbvkn0j@anonaddy.me>
2024-12-06 19:17:49 +0000

    fix

--- ui/__mocks__/ethereumjs-util.js
+++ ui/__mocks__/ethereumjs-util.js
@@ -1,9 +1,29 @@
-const zeroAddress = function() {
-  const addressLength = 20
-  const addr = zeros(addressLength)
-  return bufferToHex(addr)
-}
+// Vendored from ethereumjs-util@7.1.5
 
+/**
+ * Converts a `Buffer` into a `0x`-prefixed hex `String`.
+ *
+ * @param buf - `Buffer` object to convert
+ */
+const bufferToHex = function (buf) {
+  // buf = toBuffer(buf);
+  return `0x${buf.toString('hex')}`;
+};
+
+/**
+ * Returns a buffer filled with 0s.
+ *
+ * @param bytes - the number of bytes the buffer should be
+ */
+const zeros = function (bytes) {
+  return Buffer.allocUnsafe(bytes).fill(0);
+};
+
+const zeroAddress = function () {
+  const addressLength = 20;
+  const addr = zeros(addressLength);
+  return bufferToHex(addr);
+};
 
 module.exports = {
   zeroAddress,
